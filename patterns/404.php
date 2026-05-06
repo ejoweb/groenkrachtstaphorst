ce79323c30342945e04678ad92552db2f967a0bc
Erik Joling <info@ejoweb.nl>
2026-05-06 09:41:58 +0200

    Improve templates

--- patterns/404.php
+++ patterns/404.php
@@ -6,6 +6,10 @@
  * Inserter: no
  */
 ?>
+<!-- wp:cover {"url":"/wp-content/themes/groenkrachtstaphorst/assets/images/default-banner.jpg","dimRatio":0,"isUserOverlayColor":true,"minHeight":240,"minHeightUnit":"px","sizeSlug":"large","align":"full","layout":{"type":"default"}} -->
+<div class="wp-block-cover alignfull" style="min-height:240px"><img class="wp-block-cover__image-background size-large" alt="" src="/wp-content/themes/groenkrachtstaphorst/assets/images/default-banner.jpg" data-object-fit="cover" /><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><div class="wp-block-cover__inner-container"></div></div>
+<!-- /wp:cover -->
+
 <!-- wp:heading {"textAlign":"center","level":1,"align":"wide"} -->
 <h1 class="wp-block-heading alignwide has-text-align-center">Pagina niet gevonden</h1>
 <!-- /wp:heading -->

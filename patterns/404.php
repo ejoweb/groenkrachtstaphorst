38f58f33036a70acae2e107b151db9f34b7e119e
Erik Joling <info@ejoweb.nl>
2026-05-06 09:50:49 +0200

    Improve 404

--- patterns/404.php
+++ patterns/404.php
@@ -10,8 +10,8 @@
 <div class="wp-block-cover alignfull" style="min-height:240px"><img class="wp-block-cover__image-background size-large" alt="" src="/wp-content/themes/groenkrachtstaphorst/assets/images/default-banner.jpg" data-object-fit="cover" /><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><div class="wp-block-cover__inner-container"></div></div>
 <!-- /wp:cover -->
 
-<!-- wp:heading {"textAlign":"center","level":1,"align":"wide"} -->
-<h1 class="wp-block-heading alignwide has-text-align-center">Pagina niet gevonden</h1>
+<!-- wp:heading {"textAlign":"center","level":1,"align":"wide","style":{"elements":{"link":{"color":{"text":"var:preset|color|bruin-3"}}}},"textColor":"bruin-3"} -->
+<h1 class="wp-block-heading alignwide has-text-align-center has-bruin-3-color has-text-color has-link-color">Pagina niet gevonden</h1>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph {"align":"center"} -->
